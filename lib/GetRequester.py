925cb6da79c45aa95a4057701d09d837ccce431a
James Balaraman <james.balaraman@gmail.com>
2024-03-07 10:46:59 -0700

    Finally feel like im getting it

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        dictionary = []
+        dictionaries = json.loads(self.get_response_body())
+        for entry in dictionaries:
+            dictionary.append(entry)
+
+        return dictionary
